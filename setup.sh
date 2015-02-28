6ea054ee4f96714e1f7a1b703d6e390232430e95
Jace Bennett <jace.bennett@gmail.com>
2015-02-28 18:09:14 -0500

    let's try it

--- setup.sh
+++ setup.sh
@@ -17,15 +17,12 @@ read -p "GitHub 2FA OTP: " ghotp
 
 echo "user: $ghuser, pass: $ghpass, otp: $ghotp"
 
-#tokenresponse=$(curl --silent --user $ghuser:$ghpass --header "X-GitHub-OTP: $ghotp" --data "{\"scopes\": [\"write:public_key\"], \"note\": \"setup dev env $keyid\"}" https://api.github.com/authorizations)
-#token=$(echo $tokenresponse | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w token | sed 's/.*: //')
-#echo github oauth token: $token
-#echo curl -u $token:x-oauth-basic --data "{\"title\":\"$keyid\",\"key\":\"$pubkey\"}" https://api.github.com/user/keys
-#curl -u $token:x-oauth-basic --data "{\"title\":\"$keyid\",\"key\":\"$pubkey\"}" https://api.github.com/user/keys
-#curl -i -X GET -u "$githubuser:$githubpass" -H "'X-GitHub-OTP: $github2fa'" https://api.github.com/user
+tokenresponse=$(curl --silent --user $ghuser:$ghpass --header "X-GitHub-OTP: $ghotp" --data "{\"scopes\": [\"write:public_key\"], \"note\": \"setup dev env $keyid\"}" https://api.github.com/authorizations)
+token=$(echo $tokenresponse | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w token | sed 's/.*: //')
+curl -s -u $token:x-oauth-basic --data "{\"title\":\"$keyid\",\"key\":\"$pubkey\"}" https://api.github.com/user/keys
 
-#git clone git@github.com:bnwasteland/dotfiles.git
+git clone git@github.com:bnwasteland/dotfiles.git
 
-#cd dotfiles
-#. install.sh
-#cd ..
+cd dotfiles
+. install.sh
+cd ..
