5d48f087a7a39d421c45b67bf90102518711e186
Jace Bennett <jace.bennett@gmail.com>
2015-02-28 18:13:47 -0500

    Update setup.sh
    don't echo password, duh

--- setup.sh
+++ setup.sh
@@ -15,8 +15,6 @@ read -sp "GitHub password: " ghpass
 echo
 read -p "GitHub 2FA OTP: " ghotp
 
-echo "user: $ghuser, pass: $ghpass, otp: $ghotp"
-
 tokenresponse=$(curl --silent --user $ghuser:$ghpass --header "X-GitHub-OTP: $ghotp" --data "{\"scopes\": [\"write:public_key\"], \"note\": \"setup dev env $keyid\"}" https://api.github.com/authorizations)
 token=$(echo $tokenresponse | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w token | sed 's/.*: //')
 curl -s -u $token:x-oauth-basic --data "{\"title\":\"$keyid\",\"key\":\"$pubkey\"}" https://api.github.com/user/keys
