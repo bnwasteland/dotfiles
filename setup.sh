2c0548a1228cf1219797a5a605b02ddaded93f5d
Jace Bennett <jace.bennett@gmail.com>
2015-03-01 22:32:21 -0500

    made everything conditional

--- setup.sh
+++ setup.sh
@@ -1,47 +1,80 @@
 #!/usr/bin/env sh
 
 #ensure ssh key
-
 keyid=$USER@$HOSTNAME
-pubkeypath=$HOME/.ssh/id_rsa.pub
-
-if [[ ! -e $pubkeypath ]]
+pubkeyfile=$HOME/.ssh/id_rsa.pub
+if [[ ! -e $pubkeyfile ]]
 then
+  echo generating ssh key
   ssh-keygen -C "'$keyid'"
 fi
-pubkey=$(cat $pubkeypath)
+pubkey=$(cat $pubkeyfile)
+pubkeyvalue=$(cat $pubkeyfile | awk '{print $2}')
 
 
+#ensure github oauth2 token
+greppablejson() {
+  sed 's/\\\\\//\//g' < $1 | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g'
+}
 
-#get github oauth2 token
+ghauthfile=$HOME/.github.authorization
+ghtokenfile=$HOME/.github.token
 
-read -p "GitHub username: " ghuser
-read -sp "GitHub password: " ghpass
-echo
-read -p "GitHub 2FA OTP: " ghotp
+if [[ ! -e $ghtokenfile ]]
+then
+  if [[ ! -e $ghauthfile ]]
+  then
+    read -p "GitHub username: " ghuser
+    read -sp "GitHub password: " ghpass
+    echo
+    read -p "GitHub 2FA OTP: " ghotp
 
-tokenresponse=$(curl --silent --user $ghuser:$ghpass --header "X-GitHub-OTP: $ghotp" --data "{\"scopes\": [\"write:public_key\"], \"note\": \"setup dev env $keyid\"}" https://api.github.com/authorizations)
-token=$(echo $tokenresponse | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w token | sed 's/.*: //')
+    echo Recording full github oauth authorization in $ghauthfile
+    ghscopes="[\"repo\", \"write:public_key\"]"
+    ghauthnotes="dev env $keyid"
+    curl --silent --output $ghauthfile --user $ghuser:$ghpass --header "X-GitHub-OTP: $ghotp" --data "{\"scopes\": $ghscopes, \"note\": \"$ghauthnotes\"}" https://api.github.com/authorizations
+  fi
+  echo Recording github api oauth token in $ghtokenfile
+  greppablejson $ghauthfile | grep -w token: | sed 's/.*: //' > $ghtokenfile
+fi
+
+githubtoken=$(cat $ghtokenfile)
 
 
-
-#register ssh key on github
-
-curl -s -u $token:x-oauth-basic --data "{\"title\":\"$keyid\",\"key\":\"$pubkey\"}" https://api.github.com/user/keys
-
+#ensure ssh key registered on github
+ghkeysfile=$HOME/.github.keys
+curl -s -o $ghkeysfile -u $token:x-oauth-basic https://api.github.com/user/keys
+existingkey=$(greppablejson $ghkeysfile | grep $pubkeyvalue)
+if [[ ! $existingkey ]]
+then
+  echo Registering ssh key with github
+  curl -s -u $token:x-oauth-basic --data "{\"title\":\"$keyid\",\"key\":\"$pubkey\"}" https://api.github.com/user/keys
+else
+  echo ssh key already registered with github, skipping
+fi
+rm $ghkeysfile
 
 
 #install oh-my-zsh
-
-curl -L https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-
-
+omzdir=$HOME/.oh-my-zsh
+if [[ ! -e $omzdir ]]
+then
+  echo Installing oh-my-zsh
+  curl -s -L https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+else
+  echo oh-my-zsh already installed, skipping
+fi
 
 #install dotfiles
-
-git clone git@github.com:bnwasteland/dotfiles.git
-
-cd dotfiles
-. install.sh
-cd ..
+dotfiledir=$HOME/dotfiles
+if [[ ! -e $dotfiledir ]]
+then
+  echo Installing dotfiles
+  git clone git@github.com:bnwasteland/dotfiles.git $dotfiledir
+  pushd $dotfiledir
+  . install.sh
+  popd
+else
+  echo dotfiles already installed, skipping
+fi
 
